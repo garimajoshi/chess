fcbc69ce4e7847690e59cb1b14533d374abebe24
Garima Joshi <garimajoshi3@yahoo.in>
2013-02-13 16:33:12 +0530

    Update scripts/script.js

--- scripts/script.js
+++ scripts/script.js
@@ -47,8 +47,8 @@ $(document).ready(function() {
 
   function setupRegistrationValidate() {
 
-	$('#username').keyup( function() {
-    	$(this).removeClass('validationerror'); 
+    $('#username').keyup( function() {
+    $(this).removeClass('validationerror'); 
     });
     $('#email').keyup( function() {
     	$(this).removeClass('validationerror'); 
@@ -83,8 +83,33 @@ $(document).ready(function() {
   	});
   }
   
+  function setupLoginValidate() {
+
+    $('#lemail').keyup( function() {
+     $(this).removeClass('validationerror');
+    });
+    $('#lpassword').keyup( function() {
+     $(this).removeClass('validationerror');
+    });
+
+   $('#loginForm').submit( function () {
+   var toReturn = true;
+   var email = $('#lemail').val();
+   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) == false) {
+   toReturn = false;
+   $('#lemail').addClass('validationerror');
+   }
+   if ($('#lpassword').val() == '') {
+   toReturn = false;
+   $('#lpassword').addClass('validationerror');
+   }
+   return toReturn;
+   });
+  }
+  
   setupLoginDialog();
   setupRegistrationValidate();
+  setupLoginValidate();
 
 /*
  * ------------------------------ HEADER ENDS ----------------------------------------------------
@@ -95,130 +120,10 @@ $(document).ready(function() {
  * Contains scripts for the 'wrapper' section
  */
 
-	var fetchedContentData, currentContentObject; // currentContentObject corresponds to the content for the centre node
-	var fetchedMenuData, currentMenuObject; // currentMenuObject corresponds to the centre node in the menu
-	var fetchedNotificationData;
-
-	/* Invokes the circular menu plugin */
-  function applyCircleMenu(){
-  	console.log("applyCircleMenu");
-  	$('.nav-menu-list').circleMenu({
-		    trigger: 'hover',
-		    item_diameter: 126,
-		    circle_radius: 126,
-		    direction: 'full',
-		    delay: 2000
-	   });
-			
-       $('.someclass').each( function(){
-         var textHeight = $(this).height();
-         var nodeHeight = $(this).parent().height(); 
-         console.log( $(this).text() );
-         console.log( $(this).parent().text() );
-         $(this).css({
-           'position': 'relative',
-           'top': ( ( nodeHeight - textHeight ) / 2 ) + 'px'
-         });
-       });
-
-		/* Simulate a hover to open the menu on initialization.
-		 * Triggering just a mouseover event does not close the
-		 * menu when the mouse is idle
-		 */
-		$('.nav-menu-list').trigger('mouseover');
-		$('.nav-menu-list').trigger('mouseout');
-  }
-
-	/* Updates the content section */
-	function updateContent(key){
-		var item = fetchedMenuData[key];
-
-		/* Update section only if a new node is clicked. In other words,
-		 * do not update if the section already has contents corresponding to the node clicked
-		 */
-		  /* Fade out the contents, empty the section, update the class name associated
-			 * ( wrapper-column should always remain for layout reasons ), fill in the 
-			 * content as children and then fade back in.
-			 */
-			$('#content').fadeOut(function() {
-				var contentDiv = $('#content');
-				currentContentObject = fetchedContentData[key];
-				var heading = currentContentObject.heading;
-				var content = currentContentObject.content;
-
-		  		$('#content h1').html(heading);
-		  		$('#content-body').html(content);
-		  		contentDiv.fadeIn(function() {
-		  			$("#content-body-wrapper").mCustomScrollbar('update');
-		  		});
-			});
-	}
-
-	/* Handler for a click on a child node */
-	function clickedNode(key){
-		console.log("clickedNode");
-		var item = fetchedMenuData[key];
-
-		/* No need to reinitialize the menu if the centre node was clicked */
-		if( currentMenuObject != item ){
-			/* Open a sub-menu only if the node has children */
-			if( !$.isEmptyObject( item.childNodes ) )
-			{
-				$('.nav-menu-list').fadeOut('fast', function() {
-
-					/* Remove the complete menu list element and then recreate it.
-					   Just emptying its contents and repopulating with list items doesn't
-					   work as desired because the plugin applies styles to the menu
-					   list element and initiating the circle menu again causes problems*/
-					$('.nav-menu-list').remove();
-					$('#nav-menu').append('<ul class="nav-menu-list"></ul>');
-
-					currentMenuObject = item;
-					setUpMenu(key);
-					applyCircleMenu();
-					$('.nav-menu-list').fadeIn();
-				});
-			}
-		}
-		updateContent(key);
-	}
-
-  function setUpMenu(key){
-  	console.log("setUpMenu");
-  	var item = currentMenuObject;
-  	var centreNodeText = item.text;
- 		$('.nav-menu-list').append('<li class="centre-node" id="' + key + '" ><div class="someclass">' + centreNodeText + '</div></li>');
-
-		$.each(item.childNodes, function() {
-			var id = this.toString();
-			var nodeText = fetchedMenuData[id].text;
-			$('.nav-menu-list').append('<li class="child-node" id="' + id + '" ><div class="someclass">' + nodeText + '</div></li>');
-
-			$('#' + id).click(function() {
-				clickedNode(id);
-			});
-		});
-
-		if( item.parent != "none" ){
-			var parentId = item.parent;
-			var nodeText = fetchedMenuData[parentId].text;
-			$('.nav-menu-list').append('<li class="back-node" id="' + parentId + '" ><div class="someclass">' + 'Back to ' + nodeText + '</div></li>');
-			$('#' + parentId).click(function() {
-				clickedNode(parentId);
-			});
-		}
-  }
+	var fetchedContentData;
 
 	$.getJSON('scripts/content.json', function(data) {
 		fetchedContentData = data;
-	});
-
-	$.getJSON('scripts/menuContents.json', function(data) {
-		fetchedMenuData = data;
-		currentMenuObject = fetchedMenuData['mukti'];
-		setUpMenu('mukti');
-		applyCircleMenu();
-		updateContent('mukti');	
 	});
 
 /* ------------------------------ WRAPPER END --------------------------------------------------- */
